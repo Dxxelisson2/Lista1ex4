cbb41f8c43d4d2652b736aecd66616bb09288b81
Dxxelisson2 <130613973+Dxxelisson2@users.noreply.github.com>
2023-04-12 20:46:17 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double b;
+            double h;
+            double area;
+
+            Console.WriteLine("Calculo da area do triangulo");
+            Console.WriteLine();
+
+            Console.WriteLine("digite Valor da Base");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine("digite Valor da Altura");
+            h = double.Parse(Console.ReadLine());
+            
+            area = b * h / 2;
+            Console.WriteLine("area:"+area);
+        
+       
+        }
+    }
+}
